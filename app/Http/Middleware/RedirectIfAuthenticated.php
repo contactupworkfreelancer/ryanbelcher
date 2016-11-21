86aa444ee03999ca64cbbdcd81f01629074a7a13
pankaj monga <pankaj.monga@live.com>
2016-11-21 23:59:03 +0530

    small changes

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -17,6 +17,8 @@ class RedirectIfAuthenticated
      */
     public function handle($request, Closure $next, $guard = null)
     {
+
+      echo "<pre> Data : ".print_r(Auth::user(), TRUE)."</pre>";
         if (Auth::guard($guard)->check()) {
             return redirect('/home');
         }
