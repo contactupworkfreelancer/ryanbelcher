4d0fad6385a0a08da6e16642de69779527650e54
pankaj monga <pankaj.monga@live.com>
2016-11-22 04:02:56 +0530

    small changes

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
 use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\User;
 
 class Dashboard extends Controller
 {
