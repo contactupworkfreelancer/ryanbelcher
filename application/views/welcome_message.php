49537e9452759101f86c864695c0500646af35b7
pankaj monga <pankaj.monga@live.com>
2017-01-11 01:27:14 +0530

    add layout

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,89 +1,343 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+  <title>About us</title>
+  <meta charset="utf-8">
+  <meta name="format-detection" content="telephone=no"/>
+  <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+  <link rel="stylesheet" href="css/grid.css">
+  <link rel="stylesheet" href="css/style.css">
 
-	<style type="text/css">
+  <link rel="stylesheet" href="css/owl-carousel.css">
 
-	::selection { background-color: #E13300; color: white; }
-	::-moz-selection { background-color: #E13300; color: white; }
+  <script src="js/jquery.js"></script>
+  <script src="js/jquery-migrate-1.2.1.js"></script>
 
-	body {
-		background-color: #fff;
-		margin: 40px;
-		font: 13px/20px normal Helvetica, Arial, sans-serif;
-		color: #4F5155;
-	}
+  <!--[if lt IE 9]>
+  <html class="lt-ie9">
+  <div style=' clear: both; text-align:center; position: relative;'>
+    <a href="http://windows.microsoft.com/en-US/internet-explorer/..">
+      <img src="images/ie8-panel/warning_bar_0000_us.jpg" border="0" height="42" width="820"
+           alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."/>
+    </a>
+  </div>
+  <script src="js/html5shiv.js"></script>
+  <![endif]-->
 
-	a {
-		color: #003399;
-		background-color: transparent;
-		font-weight: normal;
-	}
-
-	h1 {
-		color: #444;
-		background-color: transparent;
-		border-bottom: 1px solid #D0D0D0;
-		font-size: 19px;
-		font-weight: normal;
-		margin: 0 0 14px 0;
-		padding: 14px 15px 10px 15px;
-	}
-
-	code {
-		font-family: Consolas, Monaco, Courier New, Courier, monospace;
-		font-size: 12px;
-		background-color: #f9f9f9;
-		border: 1px solid #D0D0D0;
-		color: #002166;
-		display: block;
-		margin: 14px 0 14px 0;
-		padding: 12px 10px 12px 10px;
-	}
-
-	#body {
-		margin: 0 15px 0 15px;
-	}
-
-	p.footer {
-		text-align: right;
-		font-size: 11px;
-		border-top: 1px solid #D0D0D0;
-		line-height: 32px;
-		padding: 0 10px 0 10px;
-		margin: 20px 0 0 0;
-	}
-
-	#container {
-		margin: 10px;
-		border: 1px solid #D0D0D0;
-		box-shadow: 0 0 8px #D0D0D0;
-	}
-	</style>
+  <script src='js/device.min.js'></script>
 </head>
 <body>
+<div class="page">
+  <!--========================================================
+                            HEADER
+  =========================================================-->
+  <header>
+    <div id="stuck_container" class="stuck_container">
+      <div class="header-section1">
+        <div class="container">
+          <div class="brand">
+            <h1 class="brand_name fl-justicons-airplane82">
+              <a href="./">MailJet</a>
+            </h1>
+          </div>
 
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+          <nav class="nav">
+            <ul class="sf-menu" data-type="navbar">
+              <li class="active">
+                <a href="./">About us </a>
+              </li>
+              <li>
+                <a href="index-1.html">Our services</a>
+                <ul>
+                  <li>
+                    <a href="#">News</a>
+                  </li>
+                  <li>
+                    <a href="#">Events</a>
+                    <ul>
+                      <li>
+                        <a href="#">Setup</a>
+                      </li>
+                      <li>
+                        <a href="#">Marketing</a>
+                      </li>
+                    </ul>
+                  </li>
+                  <li>
+                    <a href="#">Prices</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="index-2.html">Clients</a>
+              </li>
+              <li>
+                <a href="index-3.html">Careers</a>
+              </li>
+              <li>
+                <a href="index-4.html">Contacts</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </div>
 
-	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
-		<code>application/views/welcome_message.php</code>
+    <div class="header-section2">
+      <div class="container">
+        <h2>Meet The Mail Jet</h2>
+        <h3>The Best Mail Service On The Web! </h3>
+        <div class="btn-holder">
+          <a href="#" class="btn">Sign Up</a>
+          <a href="#" class="btn"><i class="fa-caret-right"></i> Take a Tour</a>
+        </div>
+      </div>
+    </div>
 
-		<p>The corresponding controller for this page is found at:</p>
-		<code>application/controllers/Welcome.php</code>
+  </header>
+  <!--========================================================
+                            CONTENT
+  =========================================================-->
+  <main>
 
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
-	</div>
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+    <section class="well">
+      <div class="container">
+        <ul class="list1 row float-fix">
+          <li class="col-md-4 col-sm-4 col-xs-6  wow fadeIn fl-bigmug-line-airplane86">
+            <h5>Jet Mailer</h5>
+            <p>MailJet's send time optimization recommends the best send time within 24 hours of your selected delivery date.</p>
+          </li>
+          <li class="col-md-4 col-sm-4 col-xs-6  wow fadeIn fl-bigmug-line-attach8">
+            <h5>Files attach</h5>
+            <p>Add files and one-to-one conversations to profiles, so you can remember who your customers are. And you can access it all </p>
+          </li>
+          <li class="col-md-4 col-sm-4 col-xs-6  wow fadeIn fl-bigmug-line-big104">
+            <h5>Geotargeting</h5>
+            <p>Access your MailJet account from your phone with MailJet Mobile. Check your reports, add notes about a contact</p>
+          </li>
+          <li class="col-md-4 col-sm-4 col-xs-6  wow fadeIn fl-bigmug-line-circular228">
+            <h5>Dashboard</h5>
+            <p>In addition to the reports that come with every account, MailJet offers plenty of advanced reporting options that you want</p>
+          </li>
+          <li class="col-md-4 col-sm-4 col-xs-6  wow fadeIn fl-bigmug-line-cellular9">
+            <h5>Responsive emails</h5>
+            <p>Our templates and forms are mobile-ready out of the box, so you don't have to worry about coding anything special to make </p>
+          </li>
+          <li class="col-md-4 col-sm-4 col-xs-6  wow fadeIn fl-bigmug-line-planetary2">
+            <h5>52 Countries</h5>
+            <p>Edit campaigns, collaborate with your team, and send from a tablet with MailJet Editor. And with Jetadeedoo,</p>
+          </li>
+        </ul>
+      </div>
+    </section>
+
+    <section class="bg-img1">
+      <div class="container">
+        <div class="row">
+            <div class="col-md-5 col-sm-6 col-xs-12  wow fadeIn">
+              <h4>MailJet helps you email the right people at the right time.</h4>
+              <p>Send automated emails based on customer behavior and preferences. Get started with pre-built Workflows or use our built-in segmentation and targeting options to build</p>
+              <a href="#" class="btn2">Sign Up</a>
+            </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="well2">
+      <div class="container">
+        <div class="row">
+            <div class="col-md-3 col-sm-3 col-xs-6  wow fadeIn">
+              <p class="num1">259<i class="fl-bigmug-line-checkmark14"></i></p>
+              <h5>Projects Completed</h5>
+            </div>
+
+            <div class="col-md-3 col-sm-3 col-xs-6  wow fadeIn">
+              <p class="num1">108<i class="fl-bigmug-line-circular220"></i></p>
+              <h5>Awards Received</h5>
+            </div>
+
+            <div class="col-md-3 col-sm-3 col-xs-6  wow fadeIn">
+              <p class="num1">10346k<i class="fl-bigmug-line-code30"></i></p>
+              <h5>Lines of Code</h5>
+            </div>
+
+            <div class="col-md-3 col-sm-3 col-xs-6  wow fadeIn">
+              <p class="num1">306<i class="fl-bigmug-line-cocktail26"></i></p>
+              <h5>Glasses of Martini</h5>
+            </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="parallax well3 wow fadeIn" data-url="images/parallax1.jpg" data-mobile="true" data-speed="1">
+      <div class="container">
+        <h4 class="mod1">Best professional tools <br>for your business</h4>
+        <a href="#" class="btn2">Sign Up</a>
+      </div>
+    </section>
+
+
+
+    <section class="well4">
+      <div class="container">
+        <h3 class="cntr">Pricing Plans</h3>
+        <div class="row marTop1">
+          <div class="col-lg-10 col-md-12 col-sm-12 col-xs-12 col-lg-offset-1 wow fadeIn">
+            <div class="pricing">
+              <div class="price-banner">
+                <h5>Basic</h5>
+                <p class="price">$29</p>
+                <p class="price2">per mouth</p>
+                <p>Perfect for a blog or personal site, contact our Sales team to discuss the best MailJet solution for your organization.</p>
+                <ul class="list2">
+                  <li>Up to 15 members</li>
+                  <li>Data export</li>
+                  <li>SSO &amp; SAML</li>
+                  <li class="disabled">Unlimited projects</li>
+                  <li class="disabled">Premium Dashboards</li>
+                  <li class="disabled">Support</li>
+                </ul>
+                <a href="#" class="btn3">select plan</a>
+              </div>
+
+              <div class="price-banner">
+                <h5>Small Company</h5>
+                <p class="price">$49</p>
+                <p class="price2">per mouth</p>
+                <p>Ideal for businesses of all sizes, contact our Sales team to discuss the best MailJet solution for your organization.</p>
+                <ul class="list2">
+                  <li>Up to 15 members</li>
+                  <li>Data export</li>
+                  <li>SSO &amp; SAML</li>
+                  <li>Unlimited projects</li>
+                  <li class="disabled">Premium Dashboards</li>
+                  <li class="disabled">Support</li>
+                </ul>
+                <a href="#" class="btn3">select plan</a>
+              </div>
+
+              <div class="price-banner active">
+                <p class="recommended">recommended</p>
+                <h5>Big Company</h5>
+                <p class="price">$59</p>
+                <p class="price2">per mouth</p>
+                <p>Ideal for heavy traffic sites, contact our Sales team to discuss the best MailJet solution for your organization.</p>
+                <ul class="list2">
+                  <li>Up to 15 members</li>
+                  <li>Data export</li>
+                  <li>SSO &amp; SAML</li>
+                  <li>Unlimited projects</li>
+                  <li>Premium Dashboards</li>
+                  <li class="disabled">Support</li>
+                </ul>
+                <a href="#" class="btn3">select plan</a>
+              </div>
+
+              <div class="price-banner">
+                <h5>Corporate</h5>
+                <p class="price">$199</p>
+                <p class="price2">per mouth</p>
+                <p>For large or complex teams, contact our Sales team to discuss the best MailJet solution for your organization.</p>
+                <ul class="list2">
+                  <li>Up to 15 members</li>
+                  <li>Data export</li>
+                  <li>SSO &amp; SAML</li>
+                  <li>Unlimited projects</li>
+                  <li>Premium Dashboards</li>
+                  <li>Support</li>
+                </ul>
+                <a href="#" class="btn3">select plan</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+    <section class="bg-img2">
+      <div class="container">
+        <h3>Beautiful E-mail Templates</h3>
+        <h5 class="normal">You can choose from 1000+ well designed professional templates</h5>
+        <a href="#" class="btn2">View All</a>
+      </div>
+    </section>
+
+    <section class="bg-img3">
+      <div class="container">
+        <h3>Testimonials</h3>
+        <div class="row">
+          <div class="col-lg-7 col-md-9 col-sm-9 col-xs-12 col-lg-offset-1 wow fadeIn">
+            <div class="owl-carousel">
+              <div class="item">
+                  <blockquote>
+                    <q>For luxury retailer, Billy Reid, MailJet is more than just a way to let customers know about new products. They offer first dibs on sales to their most engaged readers, have unique ways of deciding the time of day to send their emails, and even base new brick-and-mortar store locations on their email zip code data. We visited Florence, Ala. and spoke with Billy Reid's director of e-commerce, </q>
+                    <cite>Bob Dilan, Owner</cite>
+                  </blockquote>
+              </div>
+              <div class="item">
+                  <blockquote>
+                    <q>I would just like to say thank you for your prompt and effective service, for your friendly and professional support staff! I will recommend your expert company to all my friends. </q>
+                    <cite>Rob Smith, Client</cite>
+                  </blockquote>
+              </div>
+              <div class="item">
+                  <blockquote>
+                    <q>Hi, guys! You’ve solved my problem in no time! Your efficient services and customer care are second to none! I am very happy! I will surely share my experience with my friends! </q>
+                    <cite>Sarah Johnson, Custome</cite>
+                  </blockquote>
+              </div>
+          </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+    <section class="well5 bg2 section2">
+      <div class="container">
+        <h3>Don’t waste time! Start using!</h3>
+        <a href="#" class="btn2">Sign Up</a>
+      </div>
+    </section>
+
+
+
+
+
+
+
+
+
+  </main>
+
+  <!--========================================================
+                            FOOTER
+  =========================================================-->
+  <footer>
+    <div class="container">
+
+      <div class="social-section">
+        <p>Follow Us on Social Networks</p>
+        <ul class="inline-list">
+          <li><a href="#" class="fa-facebook-square"></a></li>
+          <li><a href="#" class="fa-linkedin-square"></a></li>
+          <li><a href="#" class="fa-instagram"></a></li>
+          <li><a href="#" class="fa-pinterest-square"></a></li>
+          <li><a href="#" class="fa-twitter-square"></a></li>
+        </ul>
+      </div>
+
+      <p class="priv-txt"><span id="copyright-year"></span> copiryght by MailJet. <a href="index-5.html">Privacy Policy</a>
+      More <a rel="nofollow" href="http://www.templatemonster.com/category/email-services-website-templates/" target="_blank">Email Services Website Templates at TemplateMonster.com</a></p>
+
+    </div>
+  </footer>
 </div>
 
+<script src="js/script.js"></script>
 </body>
 </html>
